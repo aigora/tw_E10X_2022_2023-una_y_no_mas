6722bf060fdfb00d72848d9ba0175f1a11ebcc62
Mihail <80490459+MihailDimitro@users.noreply.github.com>
2023-06-21 12:20:16 +0200

    Update Trabaj.c

--- Trabaj.c
+++ Trabaj.c
@@ -3,14 +3,20 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1000
+#define N 50
 
-// FunciÛn para limpiar el b˙fer de entrada del teclado
+typedef struct {
+    char energia[N];
+    double numeros[24];
+} Dato;
+
+int columna = 0;
+
 void clearInputBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
 
-// FunciÛn para imprimir la tabla desde un archivo CSV
 void printTable(const char* filename) {
     FILE* file = fopen(filename, "r");
 
@@ -21,7 +27,15 @@ void printTable(const char* filename) {
 
     char line[MAX_LINE_LENGTH];
 
-    // Imprimir cada lÌnea del archivo
+    // Imprimir encabezado de la tabla
+    fgets(line, sizeof(line), file);
+    printf("%s", line);
+
+    // Imprimir separador de columnas
+    fgets(line, sizeof(line), file);
+    printf("%s", line);
+
+    // Imprimir datos de la tabla
     while (fgets(line, sizeof(line), file) != NULL) {
         printf("%s", line);
     }
@@ -29,65 +43,168 @@ void printTable(const char* filename) {
     fclose(file);
 }
 
-// FunciÛn para mostrar el men˙ principal y realizar acciones basadas en la opciÛn seleccionada
+int comparardatos(const void* a, const void* b) {
+    const Dato* datoA = (const Dato*)a;
+    const Dato* datoB = (const Dato*)b;
+
+    if (datoA->numeros[columna - 1] > datoB->numeros[columna - 1]) {
+        return -1;
+    } else if (datoA->numeros[columna - 1] < datoB->numeros[columna - 1]) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void ordenarmayoramenor(const char* nombreArchivoCSV, const char* nombreArchivoTXT, int col, int year, int mes) {
+    FILE* archivoCSV = fopen(nombreArchivoCSV, "r");
+    FILE* archivoTXT = fopen(nombreArchivoTXT, "w");
+
+    if (archivoCSV == NULL || archivoTXT == NULL) {
+        printf("Error al abrir los archivos.\n");
+        return;
+    }
+
+    Dato datos[N];
+    char linea[N];
+    int indice = 0;
+
+    // Leer el archivo CSV y guardar los datos en un vector de datos
+    while (fgets(linea, N, archivoCSV) != NULL) {
+        char* token = strtok(linea, ",");
+        strncpy(datos[indice].energia, token, N);
+
+        for (int i = 0; i < 24; i++) {
+            token = strtok(NULL, ",");
+            datos[indice].numeros[i] = atof(token);
+        }
+
+        indice++;
+    }
+
+    int numDatos = indice;
+    columna = col;
+
+    // Filtrar los datos por a√±o y mes
+    Dato datosFiltrados[N];
+    int numDatosFiltrados = 0;
+    for (int i = 0; i < numDatos; i++) {
+        int mesArchivo = (columna - 1) % 12;
+        int yearArchivo = (columna - 1) / 12 + 2021;
+
+        if (yearArchivo == year && mesArchivo == mes) {
+            datosFiltrados[numDatosFiltrados] = datos[i];
+            numDatosFiltrados++;
+        }
+    }
+
+    // Ordenar los datos filtrados por n√∫mero de mayor a menor
+    qsort(datosFiltrados, numDatosFiltrados, sizeof(Dato), comparardatos);
+
+    // Mostrar los datos ordenados en la salida est√°ndar
+    printf("Energ√≠as de ese a√±o y mes ordenadas de mayor a menor:\n\n");
+    for (int i = 0; i < numDatosFiltrados; i++) {
+        printf("%s,%.2lf\n", datosFiltrados[i].energia, datosFiltrados[i].numeros[columna - 1]);
+    }
+
+    // Escribir los datos ordenados en el archivo de texto
+    for (int i = 0; i < numDatosFiltrados; i++) {
+        fprintf(archivoTXT, "%s,%.2lf\n", datosFiltrados[i].energia, datosFiltrados[i].numeros[columna - 1]);
+    }
+
+    fclose(archivoCSV);
+    fclose(archivoTXT);
+}
+
 void menuPrincipal() {
     char opcion;
-
-    printf("--- Men˙ Principal ---\n");
+    int mes, year;
+    const char* archivoCSV = "re.csv";
+    const char* archivoTXT = "energiasmayoramenorporfechas.txt";
+    printf("\n--- Menu Principal ---\n");
     printf("Opciones:\n");
-    printf("a) Realizar acciÛn A\n");
-    printf("b) Realizar acciÛn B\n");
+    printf("a) Realizar acci√≥n A\n");
+    printf("b) Realizar acci√≥n B\n");
     printf("c) Salir del programa\n");
 
-    printf("\nEscribe tu opciÛn: ");
+    printf("\nEscribe tu opci√≥n: ");
     scanf(" %c", &opcion);
     clearInputBuffer();
 
     switch (opcion) {
         case 'a':
         case 'A':
-            printf("Realizando AcciÛn A...\n");
-            // AquÌ puedes agregar el cÛdigo para la AcciÛn A
+            system("cls");
+            printf("Has elegido consultar los datos por las fechas.\n");
+            printf("Elija la fecha de la que quieres buscar datos entre los a√±os 2021-2022:\n\n");
+            printf("Introduzca el mes: ");
+
+            do {
+                scanf("%i", &mes);
+                if (mes < 1 || mes > 12) {
+                    printf("Mes incorrecto, intenta de nuevo: ");
+                }
+            } while (mes < 1 || mes > 12);
+
+            printf("Introduzca el a√±o: ");
+            do {
+                scanf("%i", &year);
+                if (year != 2021 && year != 2022) {
+                    printf("El a√±o introducido no est√° en nuestro registro de datos, intenta de nuevo: ");
+                }
+            } while (year != 2021 && year != 2022);
+
+            system("cls");
+            printf("\nLa fecha elegida es %i-%i\n", mes, year);
+
+            int columna = (year - 2021) * 12 + mes;
+
+            printf("A continuaci√≥n se muestra una lista de los datos en esta fecha %i-%i ordenados de mayor a menor.\n\n", mes, year);
+            ordenarmayoramenor(archivoCSV, archivoTXT, columna, year, mes);
+            printf("\nPulsa cualquier tecla para continuar.");
+            getchar();
+            fflush(stdin);
+            system("cls");
             break;
 
         case 'b':
         case 'B':
-            printf("Realizando AcciÛn B...\n");
-            // AquÌ puedes agregar el cÛdigo para la AcciÛn B
+            printf("\nRealizando Acci√≥n B...\n");
+            // Codigo para la Acci√≥n B
             break;
 
         case 'c':
         case 'C':
-            printf("Saliendo del programa...\n");
+            printf("\nSaliendo del programa...\n");
             exit(0);
 
         default:
-            printf("OpciÛn inv·lida. IntÈntalo de nuevo.\n");
+            printf("\nOpci√≥n inv√°lida. Int√©ntalo de nuevo.\n");
             break;
     }
 }
 
 int main() {
     printf("<<<<WELCOME TO S T A T S M A R T>>>>\n");
-    printf("\nA continucion vera los valores de los registros obtenidos entre 2021 y 2022 de nuestras energias.\nEstan separadas en aÒos y meses.");
-    printf("\n\nEstas energias son: ");
-    printf("\n1.Hidraulica");
-    printf("\n2.Turbinacion de bombeo");
-    printf("\n3.Nuclear");
-    printf("\n2.Carbon");
-    printf("\n4.Motores diesel");
-    printf("\n5.Turbina de gas");
-    printf("\n6.Turbina de vapor");
-    printf("\n7.Ciclo combinado");
-    printf("\n8.Hidreolica");
-    printf("\n9.Eolica");
-    printf("\n10.Solar fotovoltaica");
-    printf("\n11.Otras renovables");
-    printf("\n12.Cogeneracion");
-    printf("\n13.Residuos no renovables");
-    printf("\n14.Residuos renovables");
-    printf("\n15.Generacion total");
-    printf("\\nPresiona Enter para continuar...");
+    printf("\nA continuaci√≥n ver√°s los valores de los registros obtenidos entre 2021 y 2022 de nuestras energ√≠as. Est√°n separadas en a√±os y meses.");
+    printf("\n\nEstas energ√≠as son:");
+    printf("\n1. Hidr√°ulica");
+    printf("\n2. Turbinaci√≥n de bombeo");
+    printf("\n3. Nuclear");
+    printf("\n4. Carb√≥n");
+    printf("\n5. Motores diesel");
+    printf("\n6. Turbina de gas");
+    printf("\n7. Turbina de vapor");
+    printf("\n8. Ciclo combinado");
+    printf("\n9. Hidreolica");
+    printf("\n10. E√≥lica");
+    printf("\n11. Solar fotovoltaica");
+    printf("\n12. Otras renovables");
+    printf("\n13. Cogeneraci√≥n");
+    printf("\n14. Residuos no renovables");
+    printf("\n15. Residuos renovables");
+    printf("\n16. Generaci√≥n total");
+    printf("\n\nPresiona Enter para continuar...");
     while (getchar() != '\n');
 
     system("cls"); // Utiliza "cls" en Windows para limpiar la pantalla
@@ -97,7 +214,6 @@ int main() {
 
     printf("\nPresiona Enter para continuar...");
     while (getchar() != '\n');
-
     system("cls"); // Utiliza "cls" en Windows para limpiar la pantalla
 
     menuPrincipal();
