d0f75ff1b8a6b5a706bdcf8fb8b4ac72363e52b6
Mihail <80490459+MihailDimitro@users.noreply.github.com>
2023-06-23 11:22:57 +0200

    Update Trabaj.c

--- Trabaj.c
+++ Trabaj.c
@@ -52,8 +52,8 @@ void menuPrincipal() {
     int mes,year;
     printf("\n--- Menu Principal ---\n");
     printf("Opciones:\n");
-    printf("a) Ordenadar de mayor a menor las engerias\n");
-    printf("b) Dependiendo de la energia: Hacer promedio o mes mas significativo de esa energia\n");
+    printf("a) Ordenadar de mayor a menor \n");
+    printf("b) Media\n");
     printf("c) Salir del programa\n");
 
     printf("\nEscribe tu opcion: ");
@@ -63,7 +63,6 @@ void menuPrincipal() {
         case 'a':
         case 'A':
             system("cls");
-            printf("Has elegido consultar los datos por las fechas.\n");
             printf("Elija la fecha de la que quieres buscar datos entre los años 2021-2022:\n\n");
             printf("Introduzca el mes: ");
 
@@ -98,52 +97,35 @@ void menuPrincipal() {
         case 'b':
         case 'B':
             system("cls");
-            printf("\nHas elegido calcular el valor más significativo o promedio de una energia.\n");
-            printf("Elige una energía (1-16):\n");
-            printf("1. Hidráulica\n");
-            printf("2. Turbinación de bombeo\n");
-            printf("3. Nuclear\n");
-            printf("4. Carbón\n");
-            printf("5. Motores diesel\n");
-            printf("6. Turbina de gas\n");
-            printf("7. Turbina de vapor\n");
-            printf("8. Ciclo combinado\n");
-            printf("9. Hidreolica\n");
-            printf("10. Eólica\n");
-            printf("11. Solar fotovoltaica\n");
-            printf("12. Otras renovables\n");
-            printf("13. Cogeneración\n");
-            printf("14. Residuos no renovables\n");
-            printf("15. Residuos renovables\n");
-            printf("16. Generación total\n");
+            printf("Elija la fecha de la que quieres buscar datos entre los años 2021-2022:\n\n");
+            printf("Introduzca el mes: ");
 
-            int energia;
-            scanf("%d", &energia);
-            if (energia < 1 || energia > 16) {
-                printf("Energía inválida. Inténtalo de nuevo.\n");
-                break;
-            }
-            system("cls");
-            int anio;
             do {
-                printf("Elige el año:\n");
-                printf("1. 2021\n");
-                printf("2. 2022\n");
-                scanf("%d", &anio);
-                if (anio != 2021 && anio != 2022) {
-                    printf("Año invalido. Intentalo de nuevo.\n");
+                scanf("%i", &mes);
+                if (mes < 1 || mes > 12) {
+                    printf("Mes incorrecto, intenta de nuevo: ");
                 }
-            } while (anio != 2021 && anio != 2022);
+            } while (mes < 1 || mes > 12);
+
+            printf("Introduzca el año: ");
+            do {
+                scanf("%i", &year);
+                if (year != 2021 && year != 2022) {
+                    printf("El año introducido no está en nuestro registro de datos, intenta de nuevo: ");
+                }
+            } while (year != 2021 && year != 2022);
+
             system("cls");
-            printf("Elige el tipo de calculo:\n");
-            printf("1. Mes mas significativo\n");
-            printf("2. Promedio\n");
-            int tipoCalculo;
-            scanf("%d", &tipoCalculo);
-            if (tipoCalculo != 1 && tipoCalculo != 2) {
-                printf("Tipo de calculo invalido. Intentalo de nuevo.\n");
-                break;
-            }
+            printf("\nLa fecha elegida es %i-%i\n", mes, year);
+
+            int columnaa = (year - 2021) * 12 + mes;
+
+            printf("A continuación se muestra una lista de los datos en esta fecha %i-%i ordenados de mayor a menor.\n\n");
+            printf("\nPulsa cualquier tecla para continuar.");
+            getchar();
+            fflush(stdin);
+            system("cls");
+            
             break;
 
         case 'c':
